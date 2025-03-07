62e1cf4f70221882be7ac1c1f8516f4a2a50737d
Yang Weijiang <weijiang.yang@intel.com>
2025-03-08 00:41:19 +0800

    x86/fpu/xstate: Initialize guest perm with fpu_guest_cfg
    Use the new fpu_guest_cfg to initialize guest permissions.
    
    Note fpu_guest_cfg and fpu_kernel_cfg remain the same for now. So there
    is no functional change.
    
    Signed-off-by: Yang Weijiang <weijiang.yang@intel.com>
    [Gao Chao: Extrace this from the previous patch ]
    Signed-off-by: Chao Gao <chao.gao@intel.com>

--- arch/x86/kernel/fpu/core.c
+++ arch/x86/kernel/fpu/core.c
@@ -534,8 +534,15 @@ void fpstate_reset(struct fpu *fpu)
 	fpu->perm.__state_perm		= fpu_kernel_cfg.default_features;
 	fpu->perm.__state_size		= fpu_kernel_cfg.default_size;
 	fpu->perm.__user_state_size	= fpu_user_cfg.default_size;
-	/* Same defaults for guests */
-	fpu->guest_perm = fpu->perm;
+
+	/* Guest permission settings */
+	fpu->guest_perm.__state_perm	= fpu_guest_cfg.default_features;
+	fpu->guest_perm.__state_size	= fpu_guest_cfg.default_size;
+	/*
+	 * Set guest's __user_state_size to fpu_user_cfg.default_size so that
+	 * existing uAPIs can still work.
+	 */
+	fpu->guest_perm.__user_state_size = fpu_user_cfg.default_size;
 }
 
 static inline void fpu_inherit_perms(struct fpu *dst_fpu)
